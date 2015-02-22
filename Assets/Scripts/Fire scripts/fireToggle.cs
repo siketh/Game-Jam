1c715a5f788849fe8c68b003f6cb531177ba633d
Bioninja01 <vinsonjames02@gmail.com>
2015-02-21 22:03:00 -0500

    Fire scripts

---
+++ Assets/Scripts/Fire scripts/fireToggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireToggle : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		setFireOn (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setFireOn(bool haveON){
+	if (!haveON) {
+			(this.gameObject.GetComponentInChildren<Light> () as Light).enabled = false;//.renderer.enabled = false;
+			(this.gameObject.GetComponentInChildren<Light2D> () as Light2D).enabled = false;
+		} else {
+			(this.gameObject.GetComponentInChildren<Light> () as Light).enabled = true;
+			(this.gameObject.GetComponentInChildren<Light2D> () as Light2D).enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Fire scripts/playFireToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class playFireToggle : MonoBehaviour {
+
+	List<GameObject> allFires;
+	public float distanceTurnOn = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		allFires = new List<GameObject> (GameObject.FindGameObjectsWithTag ("Fire"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		allFires.Where (x => Vector3.Distance (x.transform.position, transform.position) < distanceTurnOn ).ToList ().ForEach (light => {
+			(light.GetComponent<fireToggle>() as fireToggle).setFireOn(true);
+		});
+	}
+}
